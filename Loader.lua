2f3df1be929b4455ca84c473de59611393b7d475
HoyoGey <117149371+HoyoGey@users.noreply.github.com>
2022-12-31 16:17:38 +0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -3,6 +3,6 @@ key:MakeUI({
   Title = "Drible", --// dont need to explain
   KeyLinkName = "Drible", --// the link u chose thru the bot.
   DiscordLink = "discord.gg/Pqh5cPh9R5", --// replace the numbers by your discord invite
-  ScriptLink = "https://raw.githubusercontent.com/HoyoGey/DribleHub/main/GameChecker.lua" --// this is where u put the raw link to ur source
+  ScriptLink = "https://raw.githubusercontent.com/HoyoGey/DribleHub/main/GameChecker.lua", --// this is where u put the raw link to ur source
   FileKey = "Drible.txt" --// you will need to match this in the source version.
 })
