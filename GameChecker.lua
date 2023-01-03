10cb07ecc3834e5524672b4f1ff2bcab3a8935cc
HoyoGey <117149371+HoyoGey@users.noreply.github.com>
2023-01-03 21:25:15 +0600

    Update GameChecker.lua

--- GameChecker.lua
+++ GameChecker.lua
@@ -1,7 +1,7 @@
 
 if game.PlaceId == 893973440 then
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/HoyoGey/DribleHub/main/Games/FleeTheFacility.lua'))()
-if game.PlaceId == 621129760 then
+elseif game.PlaceId == 621129760 then
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/HoyoGey/DribleHub/main/Games/KAT.lua'))()
 else
 	local Noti = loadstring(game:HttpGet "https://raw.githubusercontent.com/HoyoGey/My-Lua-Project/main/UiLibs/StrikeUiLib.txt")()
