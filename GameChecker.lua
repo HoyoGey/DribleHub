8c278f8486678ace3d35ed11b3e38f8948badc11
HoyoGey <117149371+HoyoGey@users.noreply.github.com>
2023-01-03 19:39:34 +0600

    Update GameChecker.lua

--- GameChecker.lua
+++ GameChecker.lua
@@ -3,6 +3,8 @@ local Noti = loadstring(game:HttpGet "https://raw.githubusercontent.com/HoyoGey/
 spawn(function()
 	if game.PlaceId == 893973440 then
 		loadstring(game:HttpGet('https://raw.githubusercontent.com/HoyoGey/DribleHub/main/Games/FleeTheFacility.lua'))()
+	if game.PlaceId == 621129760 then
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/HoyoGey/DribleHub/main/Games/KAT.lua'))()
 	else
 		Noti:Notif({ Name = "Game Not Supported", Content = "Check All Supported Games On Discord Server", Time = 5,Icon = "https://www.roblox.com/headshot-thumbnail/image?userId=" ..game.Players.LocalPlayer.UserId .. "&width=420&height=420&format=png" })
 	end
