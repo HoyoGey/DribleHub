0d292f2f29f2bb9d5cbb567a950a804e13442484
HoyoGey <117149371+HoyoGey@users.noreply.github.com>
2023-01-08 14:34:12 +0600

    Update GameChecker.lua

--- GameChecker.lua
+++ GameChecker.lua
@@ -3,6 +3,8 @@ if game.PlaceId == 893973440 then
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/HoyoGey/DribleHub/main/Games/FleeTheFacility.lua'))()
 elseif game.PlaceId == 621129760 then
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/HoyoGey/DribleHub/main/Games/KAT.lua'))()
+elseif game.PlaceId == 142823291 then
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/HoyoGey/DribleHub/main/Games/MM2.lua'))()
 else
 	local Noti = loadstring(game:HttpGet "https://raw.githubusercontent.com/HoyoGey/My-Lua-Project/main/UiLibs/StrikeUiLib.txt")()
 	Noti:Notif({ Name = "Game Not Supported", Content = "Check All Supported Games On Discord Server", Time = 5,Icon = "rbxassetid://11044015190" })
