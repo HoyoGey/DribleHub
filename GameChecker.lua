25380d35d1c8038d08fa2fa12dcd6ab6d448295c
HoyoGey <117149371+HoyoGey@users.noreply.github.com>
2023-01-22 11:51:35 +0600

    Update GameChecker.lua

--- GameChecker.lua
+++ GameChecker.lua
@@ -5,6 +5,8 @@ elseif game.PlaceId == 621129760 then
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/HoyoGey/DribleHub/main/Games/KAT.lua'))()
 elseif game.PlaceId == 142823291 then
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/HoyoGey/DribleHub/main/Games/MM2.lua'))()
+elseif game.PlaceId == 6284583030 or game.PlaceId == 7722306047 or game.PlaceId == 10321372166 then
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/HoyoGey/DribleHub/main/Games/PSX.lua'))()
 else
 	local Noti = loadstring(game:HttpGet "https://raw.githubusercontent.com/HoyoGey/My-Lua-Project/main/UiLibs/StrikeUiLib.txt")()
 	Noti:Notif({ Name = "Game Not Supported", Content = "Check All Supported Games On Discord Server", Time = 5,Icon = "rbxassetid://11044015190" })
